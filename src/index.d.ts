95f147a90d7a1a570256ddbe50c4bd988a11b3c8
daishi <daishi@axlight.com>
2018-10-18 17:39:57 +0900

    fix type def again

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,9 +1,9 @@
 type Callback<P> = ((props: P) => void) | undefined;
 type Options = {
-	onMount: string | undefined,
-	onUnmount: string | undefined,
+  onMount: string | undefined,
+  onUnmount: string | undefined,
 };
 export type ComposeWithOnMount =
-	<P extends {}>(onMount: Callback<P>, onUnmount?: Callback<P>, options?: Options) =>
-    React.ComponentType<P> => React.ComponentType<P>;
+  <P extends {}>(onMount: Callback<P>, onUnmount?: Callback<P>, options?: Options) =>
+    (base: React.ComponentType<P>) => React.ComponentType<P>;
 export const composeWithOnMount: ComposeWithOnMount;
