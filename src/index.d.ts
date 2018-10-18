3cc7063121a77666349b9d59392b863036eb05f4
daishi <daishi@axlight.com>
2018-10-18 17:26:17 +0900

    fix type def

--- src/index.d.ts
+++ src/index.d.ts
@@ -3,6 +3,7 @@ type Options = {
 	onMount: string | undefined,
 	onUnmount: string | undefined,
 };
-export type composeWithOnMount =
+export type ComposeWithOnMount =
 	<P extends {}>(onMount: Callback<P>, onUnmount?: Callback<P>, options?: Options) =>
     React.ComponentType<P> => React.ComponentType<P>;
+export const composeWithOnMount: ComposeWithOnMount;
